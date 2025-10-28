2774a7697afdf20960ee2f4ad24181c80ad94eb7
hocein <1hccein@gmail.com>
2025-10-28 16:38:34 +0330

    i hate this situation

--- src/components/CarouselR.tsx
+++ src/components/CarouselR.tsx
@@ -7,115 +7,53 @@ import {
   CarouselItem,
 } from '@/components/ui/carousel';
 import { Container } from './ui/Container';
-import { Button } from './ui/button';
+import { div } from 'framer-motion/client';
+
+type slidesType = {
+  id: number,
+  imgUrl: string
+}
+
+const slides: slidesType[] = [
+  { id: 1, imgUrl: "4. Financial Security.png" },
+  { id: 2, imgUrl: "5. Mobile Banking.png"},
+  { id: 3, imgUrl: "13. Online Store.png"},
+  { id: 4, imgUrl: "4. Financial Security.png" },
+  { id: 5, imgUrl: "5. Mobile Banking.png"},
+  { id: 6, imgUrl: "13. Online Store.png"},
+  { id: 7, imgUrl: "4. Financial Security.png" },
+  { id: 8, imgUrl: "5. Mobile Banking.png"},
+  { id: 9, imgUrl: "13. Online Store.png"},
+]
+  
+
+
 
 export function CarouselBasic() {
   return (
-    <Container className='bg-accent mt-10'>
-    <div className='relative w-full max-w-5xl mx-auto h-[60vh] flex items-center'>
+    <Container className='relative mt-20 bg-accent'>
+    <div className='w-full max-w-xl xl:max-w-4xl mx-auto h-[60vh] flex items-center'>
       <Carousel>
+        <div className='mb-5'>
+          <h5 className='text-2xl font-bold'>محصولات ما:</h5>
+        </div>
         <CarouselContent>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
+          
+          {slides.map((slide) => (
+          <CarouselItem key={slide.id} className='p-4 basis-full sm:basis-1/2 md:basis-1/3 xl:basis-1/4'>
+            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800 overflow-hidden'>
               <Image
-                className=''
-                src={'/4. Financial Security.png'}
+                className='pointer-events-none'
+                src={`/${slide.imgUrl}`}
                 width={500}
-                height={500}
+                height={1}
                 alt=''
               >
               </Image>
+              
             </div>
           </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Image
-                className=''
-                src={'/5. Mobile Banking.png'}
-                width={500}
-                height={500}
-                alt=''
-              >
-              </Image>
-            </div>
-          </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Image
-                className=''
-                src={'/13. Online Store.png'}
-                width={500}
-                height={500}
-                alt=''
-              >
-              </Image>
-            </div>
-          </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Image
-                className=''
-                src={'/16. Office Building.png'}
-                width={500}
-                height={500}
-                alt=''
-              >
-              </Image>
-            </div>
-          </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Image
-                className=''
-                src={'/20. Financial Report.png'}
-                width={500}
-                height={500}
-                alt=''
-              >
-              </Image>
-            </div>
-          </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Image
-                className=''
-                src={'/4. Financial Security.png'}
-                width={500}
-                height={500}
-                alt=''
-              >
-              </Image>
-            </div>
-          </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Image
-                className=''
-                src={'/4. Financial Security.png'}
-                width={500}
-                height={500}
-                alt=''
-              >
-              </Image>
-            </div>
-          </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Image
-                className=''
-                src={'/4. Financial Security.png'}
-                width={500}
-                height={500}
-                alt=''
-              >
-              </Image>
-            </div>
-          </CarouselItem>
-          <CarouselItem className='p-4 md:basis-1/3'>
-            <div className='flex aspect-square items-center justify-center border border-zinc-200 dark:border-zinc-800'>
-              <Button variant={'link'}>مشاهده ی بیشتر</Button>
-            </div>
-          </CarouselItem>
+          ))}
         </CarouselContent>
         <CarouselNavigation alwaysShow />
         <CarouselIndicator />
